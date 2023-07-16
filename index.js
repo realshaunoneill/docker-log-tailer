79969315c44d021dfb1e85e54cf2f13ca6ff32d4
Shaun O'Neill <shaunoneill7@gmail.com>
2023-07-16 21:26:54 +0100

    Update log tailer format

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Logger = require('r7insight_node');
 const SEND_LOGS = process.env.SEND_LOGS;
 const LOG_TOKEN = process.env.LOG_TOKEN;
 const LOG_REGION = process.env.LOG_REGION;
-const SILENT_MODE = process.env.SILENT_MODE;
+
+const LT_PREFIX = 'lt-';
 
 let log;
 
@@ -24,19 +25,16 @@ if (!docker) {
 }
 
 const handleLogs = ({names, log, image}) => {
-    if (!SEND_LOGS || !LOG_TOKEN || !log) {
-        return  console.log(`Container: ${names} - Log: ${log}`);
-    }
-
+    
+    console.log(`${LT_PREFIX} Container: ${names} - Log: ${log}`);
     log.info({image, names, log});
 };
 
 const run = async () => {
     const containers = await docker.listContainers();
-    if (!SILENT_MODE) {
-        console.log('Watching containers for logs: ', containers.length);
-    }
-    console.log(containers);
+
+    console.log(`${LT_PREFIX} Watching containers for logs: `, containers.length);
+
     containers.forEach(async ({Id, Names, Image}) => {
         const dockerContainerInstance = docker.getContainer(Id);
 
@@ -48,8 +46,10 @@ const run = async () => {
           });
         
           logStream.on('data', (chunk) => {
-            const logLine = chunk.toString('utf8');
-            handleLogs({names: Names, log: logLine, image: Image});
+            const logLine = chunk.toString('utf8').replace(/[^\x00-\x7F]/g, "");
+            if (!logLine.startsWith(LT_PREFIX)) {
+                handleLogs({names: Names, log: logLine, image: Image});
+            }
           });
     });
     
